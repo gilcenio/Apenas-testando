d57bc687ebbd7685a8559d8e957b44fb86bd07c9
gilceniosantos <gilcenio@1mage.com>
2025-05-22 13:02:19 -0300

    first commit

---
+++ src/components/scrollbar/types.ts
@@ -0,0 +1,14 @@
+import type { Theme, SxProps } from '@mui/material/styles';
+
+// ----------------------------------------------------------------------
+
+export type ScrollbarProps = &
+  React.ComponentProps<'div'> & {
+    sx?: SxProps<Theme>;
+    fillContent?: boolean;
+    slotProps?: {
+      wrapperSx?: SxProps<Theme>;
+      contentSx?: SxProps<Theme>;
+      contentWrapperSx?: SxProps<Theme>;
+    };
+  };
---
+++ src/types/simplebar-react.d.ts
@@ -0,0 +1,7 @@
+// src/types/simplebar-react.d.ts
+declare module 'simplebar-react' {
+  // reexporta tudo do arquivo de tipagens oficial
+  export * from 'simplebar-react/dist/index';
+  // exporta o default também
+  export { default } from 'simplebar-react/dist/index';
+}
